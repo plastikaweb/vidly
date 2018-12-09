7d5b5866ceb2af1b727fb8f3896b15f6d5a559a1
Carlos Matheu <info@plastikaweb.com>
2018-12-09 12:26:04 +0100

    add packages morgan (to log HTTP request on production) and config (to set environments)

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
+import * as config from 'config';
 import * as express from 'express';
 import * as helmet from 'helmet';
-import { GenresRoutes, HomeRoutes } from './routes/';
+import * as morgan from 'morgan';
+import { GenresRoutes, HomeRoutes } from './routes';
 
 class App {
   public app: express.Application;
@@ -8,9 +10,13 @@ class App {
   constructor() {
     this.app = express();
     this.config();
+    console.log('NAME:', config.get('name'));
   }
 
   private config() {
+    if (this.app.get('env') === 'development') {
+      this.app.use(morgan('dev'));
+    }
     this.app
       .set('port', process.env.PORT || 4000)
       .use(express.json())
